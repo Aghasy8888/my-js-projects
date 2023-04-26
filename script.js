a694e4d6f4086ded677163e4b4f954f5b3981239
Aghasy8888 <aghasghazaryan8888@gmail.com>
2023-04-26 13:05:09 +0400

    calculator with sqrt.

--- script.js
+++ script.js
@@ -6,24 +6,63 @@
   const deleteSingle = document.querySelector(".btn-deleteSingle");
   const equal = document.querySelector(".btn-equal");
   const point = document.querySelector(".point");
+  const squareRoot = document.querySelector(".square-root");
   const allButtons = document.querySelectorAll(".all");
   screen.value = "0";
   let willBeEmptyStr = "."; //after pressing an action
   let sqrtClickedFirstTime = true;
+  let executeSqrt = false;
   const actionsString = "+-*/√%";
+  const numbersString = "0123456789";
+
+  function squareRootTheStr(str) {
+    let result = Math.sqrt(
+      parseFloat(str.slice(str.search("√") + 1, str.length))
+    );
+    return str.replace(
+      "√" + str.slice(str.search("√") + 1, str.length),
+      result
+    );
+  }
+
+  squareRoot.addEventListener("click", (e) => {
+    equal.disabled = false;
+    clickedFirstTime = true;
+    if (sqrtClickedFirstTime) {
+      let value = e.target.dataset.num;
+      if (screen.value === "0") {
+        screen.value = value;
+      } else if (
+        numbersString.includes(screen.value[screen.value.length - 1])
+      ) {
+         screen.value += '*' + value;
+      } else {
+        screen.value += value;
+      }
+    }
+
+    if (executeSqrt) {
+      screen.value = squareRootTheStr(screen.value) + "*√";
+      executeSqrt = true;
+    }
+    sqrtClickedFirstTime = false;
+    willBeEmptyStr = ".";
+  });
 
   actions.forEach((action) => {
     action.addEventListener("click", (e) => {
       equal.disabled = false;
       clickedFirstTime = true;
-      if (e.target.innerHTML !== "√") {
-        sqrtClickedFirstTime = true;
-      }
-
+      sqrtClickedFirstTime = true;
+      executeSqrt = false;
       let value = e.target.dataset.num;
+      screen.value += value;
 
-      if (actionsString.includes(screen.value[screen.value.length - 1])) {
-        screen.value = screen.value.substring(0, screen.value.length - 1);
+      // if (actionsString.includes(screen.value[screen.value.length - 1])) {
+      //   screen.value = screen.value.substring(0, screen.value.length - 1);
+      // }
+      if (screen.value.includes("√")) {
+        screen.value = squareRootTheStr(screen.value) + value;
       }
 
       willBeEmptyStr = ".";
@@ -33,8 +72,8 @@
   buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
       let value = e.target.dataset.num;
-      sqrtClickedFirstTime = false;
-
+      if (screen.value.includes("√")) executeSqrt = true;
+      sqrtClickedFirstTime = true;
       if (screen.value !== "0") {
         screen.value += value;
       } else {
@@ -61,6 +100,7 @@
   });
 
   equal.addEventListener("click", (e) => {
+    screen.value = squareRootTheStr(screen.value);
     if (screen.value === "") {
       screen.value = "Please Enter";
     } else {
@@ -78,7 +118,7 @@
     } else {
       clickedFirstTime = false;
     }
-
+    executeSqrt = false;
     sqrtClickedFirstTime = true;
     willBeEmptyStr = ".";
   });
@@ -86,6 +126,8 @@
   clear.addEventListener("click", (e) => {
     screen.value = "0";
     clickedFirstTime = true;
+    sqrtClickedFirstTime = true;
+    executeSqrt = false;
   });
 
   deleteSingle.addEventListener("click", (e) => {
@@ -94,6 +136,13 @@
     } else if (actionsString.includes(screen.value[screen.value.length - 1])) {
       willBeEmptyStr = ".";
     }
+    if (screen.value[screen.value.length - 1] === "√") {
+      sqrtClickedFirstTime = true;
+      executeSqrt = false;
+    }
+    //   else if (actionsString.includes(screen.value[screen.value.length - 1])) {
+    //    willBeEmptyStr = ".";
+    //  }
     screen.value = screen.value.slice(0, screen.value.length - 1);
     if (!screen.value) {
       screen.value = "0";
